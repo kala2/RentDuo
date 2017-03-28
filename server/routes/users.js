a9ef7758adbff188f846154dd83b151dfd2bb4fa
kala1991 <themis.bo@gmail.com>
2017-03-29 00:38:18 +0300

    front end validations

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,25 +1,49 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
 import User from '../models/user';
+import isEmpty from 'lodash/isEmpty';
 
 let router = express.Router();
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
+
+
+  return User.query({
+    where: ({ email: data.email }),
+    orWhere: { username: data.username }
+  }).fetch().then(user => {
+      if (user) {
+        if (user.get('username') === data.username) {
+          errors.username = 'There is a user with this name. Please pick another!';
+        }
+        if (user.get('email') === data.email) {
+          errors.email = 'There is a user with this email. Please pick another!';
+        }
+      }
+      return {
+        errors,
+        isValid: isEmpty(errors)
+      };
+  })
+}
+
 router.post('/', (req, res) => {
-  const { errors, isValid } = validateInput(req.body);
+  validateInput(req.body, commonValidations).then(({ errors, isValid}) => {
+    if(isValid) {
+      const { username, email, password } = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
 
-  if(isValid) {
-    const { username, email, password } = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
-
-    User.forge({
-      username, email, password_digest
-    }, { hasTimestamps: true }).save()
-      .then( user => res.json({ success: true }))
-      .catch( err => res.status(500).json({ error: err }));
-  } else {
-    res.status(400).json(errors);
-  }
+      User.forge({
+        username, email, password_digest
+      }, { hasTimestamps: true }).save()
+        .then( user => res.json({ success: true }))
+        .catch( err => res.status(500).json({ error: err }));
+    } else {
+      res.status(400).json(errors);
+    }
+  });
 });
 
 export default router;
