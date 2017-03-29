ec35813576f708c8d379dcd84cdf8f694c20a465
kala1991 <themis.bo@gmail.com>
2017-03-29 13:52:48 +0300

    Login Form

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -12,6 +12,7 @@ class NavigationBar extends React.Component {
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav navbar-right">
                     <li><Link to="/signup" className="navbar-brand">Sign up</Link></li>
+                    <li><Link to="/login" className="navbar-brand">Login</Link></li>
               </ul>
             </div>
           </div>
--- client/routes.js
+++ client/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import SignupPage from './components/signup/SignupPage';
+import LoginPage from './components/login/LoginPage';
 import Greetings from './components/Greetings';
 
 export default (
       <Route path="/" component={App}>
           <IndexRoute component={Greetings} />
-          <Route path="/signup" component={SignupPage} />
+          <Route path="signup" component={SignupPage} />
+          <Route path="login" component={LoginPage} />
       </Route>
 )
